7af51f11fac48a786439f9f80ee48b285b77622e
Hendrik Tekayadi <hendrik@popsical.com>
2017-04-20 22:05:23 +0800

    Connect Playlist UI to a customer view controller

---
+++ playlist/PlaylistViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PlaylistViewController.swift
+//  playlist
+//
+//  Created by Popsical on 20/4/17.
+//  Copyright © 2017 htekayadi. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
