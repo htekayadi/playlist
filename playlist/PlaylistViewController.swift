f7b3823fc94ceda44cea6232ade957a200606b95
Hendrik Tekayadi <hendrik@popsical.com>
2017-04-20 22:52:53 +0800

    Connect Playlist UI to a custom view controller

--- playlist/PlaylistViewController.swift
+++ playlist/PlaylistViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 
 class PlaylistViewController: UIViewController {
 
+    @IBOutlet weak var coverImage0: UIImageView!
+    @IBOutlet weak var coverImage1: UIImageView!
+    @IBOutlet weak var coverImage2: UIImageView!
+    @IBOutlet weak var coverImage3: UIImageView!
+    @IBOutlet weak var coverImage4: UIImageView!
+    @IBOutlet weak var coverImage5: UIImageView!
+    @IBOutlet weak var coverImage6: UIImageView!
+    @IBOutlet weak var coverImage7: UIImageView!
+    @IBOutlet weak var coverImage8: UIImageView!
+    @IBOutlet weak var coverImage9: UIImageView!
+    @IBOutlet weak var coverImage10: UIImageView!
+    @IBOutlet weak var coverImage11: UIImageView!
+    
+    var coverImages: [UIImageView]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        coverImages = [coverImage0, coverImage1, coverImage2, coverImage3, coverImage4, coverImage5, coverImage6, coverImage7, coverImage8, coverImage9, coverImage10, coverImage11]
+        updateUI()
     }
 
+    func updateUI() {
+        for coverImage in coverImages {
+            coverImage.image = UIImage(named: "With The Beatles")
+        }
+    }
 }
