a139546004f2c99697654a403df53b41be430389
Hendrik Tekayadi <hendrik@popsical.com>
2017-04-22 00:06:52 +0800

    Create a Customer Controller for Album View

---
+++ playlist/AlbumViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AlbumViewController.swift
+//  playlist
+//
+//  Created by Popsical on 21/4/17.
+//  Copyright © 2017 htekayadi. All rights reserved.
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController {
+
+    var album: AlbumLibrary?
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var albumCoverImageView: UIImageView!
+    @IBOutlet weak var descriptionTextView: UITextView!
+
+}
